f2fd316d048be391bfd622929970f23661d42612
Thomas Hanna <thomasjhanna@gmail.com>
2018-06-16 22:27:17 +0100

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,14 +4,24 @@ const request = require('supertest');
 const { app } = require('./../server.js');
 const { Todo } = require('./../models/todo.js');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-        done();
+        return Todo.insertMany(todos);
+    }).then(() => {
+        return done();
     })
 });
-
+  
 
 describe('POST /todos', () => {
+    
     it('should create a new todo', (done) => {
         let text = 'Test todo text';
 
@@ -29,7 +39,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text:text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -51,7 +61,7 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     return done(e);
@@ -60,6 +70,14 @@ describe('POST /todos', () => {
     })
 });
 
-// describe('GET /todos', ()=>{
-//     it('should return all todos')
-// })
+describe('GET /todos', ()=>{
+    it('should return all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todos.length).toBe(2) // do not need to use the .end call as we're not doing anything asyncronously
+            })
+            .end(done);
+    });
+});
