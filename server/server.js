2ab607f19b1ce0a51c3adef539b8cb5cd9119413
Thomas Hanna <thomasjhanna@gmail.com>
2018-06-20 18:31:34 +0100

    Add GET /users/me and authentication

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose.js');
 const { Todo } = require('./models/todo.js');
 const { User } = require('./models/user.js');
+const { authenticate } = require('./middleware/authenticate.js')
 
 let app = express();
 const port = process.env.PORT;
@@ -119,6 +120,11 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
